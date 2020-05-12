53947d2e5237d413a779f740f1be7c312f3ad2a7
Jakub <jakubkozlowicz10@gmail.com>
2020-05-12 09:06:08 +0200

    [BUGFIX] Corrected uchar8 type definition

--- Coil/Source/Coil/Utilities/Types.h
+++ Coil/Source/Coil/Utilities/Types.h
@@ -1,23 +1,23 @@
 #pragma once
 #include <cstdint>
 
-typedef uint8_t		byte;
+typedef uint8_t			byte;
 
-typedef char		char8;
-typedef unsigned	uchar8;
+typedef char			char8;
+typedef unsigned char	uchar8;
 
-typedef int8_t		int8;
-typedef uint8_t		uint8;
-typedef int16_t		int16;
-typedef uint16_t	uint16;
-typedef int32_t		int32;
-typedef uint32_t	uint32;
-typedef int64_t		int64;
-typedef uint64_t	uint64;
+typedef int8_t			int8;
+typedef uint8_t			uint8;
+typedef int16_t			int16;
+typedef uint16_t		uint16;
+typedef int32_t			int32;
+typedef uint32_t		uint32;
+typedef int64_t			int64;
+typedef uint64_t		uint64;
 
-typedef float		float32;
-typedef double		float64;
-typedef long double	float128;
+typedef float			float32;
+typedef double			float64;
+typedef long double		float128;
 
 union Ambiguous
 {
