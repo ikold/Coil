db8be9209df4d2cad5f6040a9e638585b6f60ebf
Jakub <jakubkozlowicz10@gmail.com>
2020-06-28 18:24:58 +0200

    [FEATURE] Information about OpenGL context is logged

--- Coil/Source/Platform/OpenGL/OpenGLContext.cpp
+++ Coil/Source/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,21 @@ namespace Coil
 		glfwMakeContextCurrent(WindowHandle);
 		const int32 status = gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
 		CL_CORE_ASSERT(status, "GLAD initialization failed!");
+
+		const RString<> vendor   = String(reinterpret_cast<const char8*>(glGetString(GL_VENDOR)));
+		const RString<> renderer = String(reinterpret_cast<const char8*>(glGetString(GL_RENDERER)));
+		const RString<> version  = String(reinterpret_cast<const char8*>(glGetString(GL_VERSION)));
+
+		Logger::Info(PString("OpenGL Info:\n\tVendor: %R\n\tRenderer: %R\n\tVersion: %R", &vendor, &renderer, &version));
+
+#ifdef CL_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		CL_CORE_ASSERT(versionMajor > 4 || versionMajor == 4 && versionMinor >= 5, "OpenGL 4.5 or newer required!");
+#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
